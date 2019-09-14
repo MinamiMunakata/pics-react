ae7d8b74300d9d35d0074f9b33f418eed62216f0
Minami Munakata <june28munazo@gmail.com>
2019-09-14 20:48:41 +0900

    Making Api requests

---
+++ src/api/unsplush.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+export default axios.create({
+  baseUrl: "https://api.unsplash.com",
+  headers: {
+    Authorization:
+      "Client-ID c099620d221e04e26456ac1a8ab6db697e7438efaa42cc25f7fc32b8212cde99"
+  }
+});
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,28 @@
 import React from "react";
+import unsplash from "../api/unsplash";
+import axios from "axios";
 import SearchBar from "./SearchBar";
 
-const App = () => {
-  return (
-    <div className="ui container" style={{ marginTop: "10px" }}>
-      <SearchBar />
-    </div>
-  );
-};
+class App extends React.Component {
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    console.log("App: " + term);
+    const response = await axios.get("/search/photos", {
+      params: { query: term }
+    });
+
+    this.setState({ images: response.data.results });
+  };
+
+  render() {
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length}
+      </div>
+    );
+  }
+}
 
 export default App;
